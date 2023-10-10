ff4f3e6d8ccadf542f318bc59b8cff333b9ab605
Kyrsten Júnior <kyrstenjr@gmail.com>
2023-10-10 00:54:16 -0300

    inserção de media querys e arquivos .responsive

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.scss']
+  styleUrls: ['./big-card.component.scss', './big-card.responsive.component.scss']
 })
 export class BigCardComponent {
 
