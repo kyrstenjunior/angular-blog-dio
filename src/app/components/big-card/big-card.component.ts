65e44de677eb57cde85c4dfa37d835712b30b571
Kyrsten Júnior <kyrstenjr@gmail.com>
2023-10-10 23:28:01 -0300

    finalização do projeto

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,7 +7,8 @@ import { Component, Input } from '@angular/core';
 })
 
 export class BigCardComponent {
-  photoCover: string = "https://miro.medium.com/v2/resize:fit:1400/1*Aaf_dhLVmGSB0l1zM_09PQ.png";
+  photoCover: string = "https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg";
   @Input() cardTitle: string = ""; // passado direto no componente (como as props do React)
   cardDescription: string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit error repellat quisquam aperiam impedit qui iste harum. Maiores odio quaerat repellendus, autem tempore eaque voluptas, consequuntur cupiditate soluta quisquam unde!";
+  @Input() Id: string = "0";
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "image": "https://miro.medium.com/v2/resize:fit:1400/1*R1mfXLP9edcArZXwmGbGag.jpeg",
+    "title": "Saiu a nova atualização do Angular Js!",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet quod laboriosam aut sit nihil, magni incidunt illum molestiae, voluptate rem, earum tempora iste nam optio ipsa ut a maiores!"
+  },
+  {
+    "id": "2",
+    "image": "https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/06/VUE.png",
+    "title": "Saiu a nova atualização do Vue Js!",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet quod laboriosam aut sit nihil, magni incidunt illum molestiae, voluptate rem, earum tempora iste nam optio ipsa ut a maiores!"
+  },
+  {
+    "id": "3",
+    "image": "https://legacy.reactjs.org/logo-og.png",
+    "title": "Saiu a nova atualização do React Js!",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet quod laboriosam aut sit nihil, magni incidunt illum molestiae, voluptate rem, earum tempora iste nam optio ipsa ut a maiores!"
+  },
+  {
+    "id": "4",
+    "image": "https://blog.schoolofnet.com/wp-content/uploads/2019/01/php-leader-1024x524.png",
+    "title": "Saiu a nova atualização do PHP!",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor amet quod laboriosam aut sit nihil, magni incidunt illum molestiae, voluptate rem, earum tempora iste nam optio ipsa ut a maiores!"
+  }
+]
